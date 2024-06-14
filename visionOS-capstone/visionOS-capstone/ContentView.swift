e7eb3be9fb5a513ab47c6686977e307ae09874aa
Jennifer Sipila <sipila.jen@gmail.com>
2024-06-14 16:21:24 -0400

    Add xcode project

---
+++ visionOS-capstone/visionOS-capstone/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  visionOS-capstone
+//
+//  Created by Jen Sipila on 6/14/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ visionOS-capstone/visionOS-capstone/visionOS_capstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  visionOS_capstoneApp.swift
+//  visionOS-capstone
+//
+//  Created by Jen Sipila on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct visionOS_capstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
